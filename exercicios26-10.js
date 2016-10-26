41378ab73c76b469d3fa3d389864897e99068dae
pkarime <pethallakarimebrito@gmail.com>
2016-10-26 11:46:12 -0200

    Exercícios aula de JS dia 26/10

---
+++ exercicios26-10.js
@@ -0,0 +1,93 @@
+/*Exercício 1*/
+var numberTwo = 2;
+var numberFour = 4;
+
+numberTwo + numberFour 
+
+var v_9 = 9;
+var v_3 = 3;
+
+v_9 / v_3
+
+var number_5 = 5;
+var number_1 = 1;
+
+number_5 - number_1
+
+/*Exercício 2*/
+3>5
+false
+5>0
+true
+5>5
+false
+0>=1
+false
+1>-10
+
+/*Exercício 3*/
+true
+(10+40)>=(50-10)
+true
+1>-10
+true
+(3>5)&&(5>6)
+false
+(5<0)&&(0==0)
+false
+(5 !=4)||(4>4)
+true
+
+/*Exercício 4*/
+var pethalla= 3;
+var karime= 2;
+
+if (pethalla>karime) {
+    console.log ("É maior");
+};
+
+/*Exercício 5*/
+var v_A= 6;
+var v_B= 7;
+
+if (v_B>v_A){
+    console.log ("É maior");
+} else if (v_A>v_B) {console.log ("É menor");};
+
+/*Exercício 6*/
+prompt ("v_N=");
+"30"
+prompt ("v_M=");
+"40"
+alert ("N + M = 70");
+
+/*Exercício 7*/ 
+var v_idade= prompt ("Quantos anos você tem?");
+
+if (v_idade > 18) {alert ("Você já tem idade para tirar carteira");}else{alert ("Você ainda não pode dirigir");};
+
+/*Exercício 8*/
+var v_nome= prompt ("Qual seu nome?*");
+var v_sexo= prompt ("Qual seu sexo?");
+
+if (v_sexo == "feminino") {alert ("Bem-vinda" + v_nome);}else{alert("Bem-vindo" + v_nome);};
+
+/*Exercício 9*/
+var h_hora= prompt ("Que horas são?");
+
+if  ((h_hora == 0) || (h_hora <=19)){
+    alert ("Ainda é dia");
+}
+
+else if ((h_hora == 20) || (h_hora <=23)) {
+    alert ("Está a noite");
+}
+
+else{
+    alert ("horario incorreto");
+};
+
+/*Exercício 10
+upload para o git
+corrigir das coleguinhas <3
+*/
